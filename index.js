7d4ca9ea56bd88728d5f94d51bc8ca15167ba13b
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-01 22:16:28 +0545

    ui and file system for task

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const express=require("express")
+const { log } = require("console");
+const express=require("express");
 
+const fs = require('fs');
 const app=express();
 const path=require("path")
 
@@ -10,11 +12,20 @@ app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get("/",function(req,res){
-    res.render('index');
+    fs.readdir(`./files`,function(err,files){
+        res.render('index',{files:files});
+        
+    })
+    
 })
 
-app.get("/profile/:username",function(req,res){
-    res.send(`welcome ${req.params.username}`)
+app.post("/create",function(req,res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.description,function(err){
+        res.redirect("/")
+    })
+    
 })
+
+
 
 app.listen(3000);
