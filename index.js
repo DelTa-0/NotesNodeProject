d6bb49942454a4a7032742aa8dd7457ec12173da
DelTa-0 <aryalmadhave123@gmail.com>
2025-06-01 20:30:51 +0545

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require("express")
+
+const app=express();
+const path=require("path")
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname,'public')));
+
+app.get("/",function(req,res){
+    res.render('index');
+})
+
+app.get("/profile/:username",function(req,res){
+    res.send(`welcome ${req.params.username}`)
+})
+
+app.listen(3000);
